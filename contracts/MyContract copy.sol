01a3b1c6a08dc7f1467ba871e46850a01776976e
Mar.io <34627453+Maar-io@users.noreply.github.com>
2023-11-17 17:00:51 +0100

    stale branch

---
+++ contracts/MyContract copy.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import "@thirdweb-dev/contracts/base/ERC1155Drop.sol";
+
+contract MyContract is ERC1155Drop {
+    // mapping evolution id to base token id
+    mapping(uint256 => uint256) public baseEvolution;
+    // mapping boolean if token is evolution
+    mapping(uint256 => bool) public isEvolution;
+
+    uint public constant EVOLUTION_PAYMENT = 2;
+    uint public constant ENERGY_TOKEN_ID = 0;
+
+    constructor(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol,
+        address _royaltyRecipient,
+        uint128 _royaltyBps,
+        address _primarySaleRecipient
+    )
+        ERC1155Drop(
+            _defaultAdmin,
+            _name,
+            _symbol,
+            _royaltyRecipient,
+            _royaltyBps,
+            _primarySaleRecipient
+        )
+    {}
+
+    function _transferTokensOnClaim(
+        address _receiver,
+        uint256 _tokenId,
+        uint256 _quantity
+    ) internal virtual override {
+        // Claiming evolved token requires payment/burning in energy token
+        if (isEvolution[_tokenId]){
+            _verifyEvolutionCondition(_receiver);
+            // burn base egg
+            _burn(_receiver, baseEvolution[_tokenId], 1);
+            // burn energy token
+            _burn(_receiver, ENERGY_TOKEN_ID, EVOLUTION_PAYMENT);
+        }
+        else {
+            // if not evolution, then just mint energy token
+            _mint(_receiver, ENERGY_TOKEN_ID, 1, "");
+        }
+
+        _mint(_receiver, _tokenId, _quantity, "");
+    }
+
+    function setEvolution(
+        uint256 _baseId,
+        uint256 _evolutionId
+    ) external onlyOwner(){
+        baseEvolution[_evolutionId] = _baseId;
+        isEvolution[_evolutionId] = true;
+    }
+
+    function _verifyEvolutionCondition(
+        address _receiver
+    ) internal virtual {
+        require(
+            balanceOf[_receiver][ENERGY_TOKEN_ID] >= EVOLUTION_PAYMENT,
+            "Gachami: must own enough energy tokens to claim evolution"
+        );
+    }
+}
