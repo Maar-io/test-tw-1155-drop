209b55bb6a04441b052d5c70974a6b74fa66eb15
Mar.io <mario@stake.co.jp>
2023-11-20 18:19:34 +0100

    added 2 fail tests

--- test/test1.ts
+++ test/test1.ts
@@ -5,6 +5,7 @@ import { ethers } from "hardhat";
 const ENERGY_TOKEN_ID = 0;
 const BASE_TOKEN_ID = 1;
 const EVOLVED_TOKEN_ID = 2;
+const ETH_NATIVE_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 
 type AllowlistProof = {
   proof: string[];
@@ -35,7 +36,7 @@ const claimConditions: {
       quantityLimitPerWallet: 10,
       merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
       pricePerToken: ethers.parseEther("0"),
-      currency: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+      currency: ETH_NATIVE_ADDRESS,
       metadata: "ipfs://QmVu98eczZRpSYcF3UKYRDkHsM2RMQR62KUYmk29UDbWTP/0",
     }
   ],
@@ -72,7 +73,7 @@ describe("Token contract", function () {
     const condition0 = await gachaContract.setClaimConditions(ENERGY_TOKEN_ID, claimConditions._conditions[0], false);
     await condition0.wait();
     const res = await gachaContract.claimCondition(0);
-    expect(res[6]).to.equal("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
+    expect(res[6]).to.equal(ETH_NATIVE_ADDRESS);
 
     // set claim conditions for token BASE_TOKEN_ID
     const condition1 = await gachaContract.setClaimConditions(BASE_TOKEN_ID, claimConditions._conditions[0], false);
@@ -81,6 +82,8 @@ describe("Token contract", function () {
     // set claim conditions for token BASE_TOKEN_ID
     const condition2 = await gachaContract.setClaimConditions(EVOLVED_TOKEN_ID, claimConditions._conditions[0], false);
     await condition2.wait();
+
+    await gachaContract.setEvolution(BASE_TOKEN_ID, EVOLVED_TOKEN_ID);
 
   });
 
@@ -92,31 +95,69 @@ describe("Token contract", function () {
       proof: ["0x0000000000000000000000000000000000000000000000000000000000000000"],
       quantityLimitPerWallet: 100,
       pricePerToken: ethers.parseEther("0"),
-      currency: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+      currency: ETH_NATIVE_ADDRESS
     };
-    await gachaContract.claim(account1.address, ENERGY_TOKEN_ID, "2", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", 0, myAllowlistProof, "0x");
+    await gachaContract.claim(account1.address, ENERGY_TOKEN_ID, "2", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x");
     expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(2);
     expect(await gachaContract.balanceOf(account1.address, ENERGY_TOKEN_ID)).to.equal(2);
 
     // user claims base token
-    await gachaContract.claim(account1.address, BASE_TOKEN_ID, "1", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", 0, myAllowlistProof, "0x");
+    await gachaContract.claim(account1.address, BASE_TOKEN_ID, "1", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x");
     expect(await gachaContract.totalSupply(BASE_TOKEN_ID)).to.equal(1);
     expect(await gachaContract.balanceOf(account1.address, BASE_TOKEN_ID)).to.equal(1);
 
     // user claims evolved token
-    await gachaContract.claim(account1.address, EVOLVED_TOKEN_ID, "1", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", 0, myAllowlistProof, "0x");
+    await gachaContract.claim(account1.address, EVOLVED_TOKEN_ID, "1", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x");
     expect(await gachaContract.totalSupply(EVOLVED_TOKEN_ID)).to.equal(1);
     expect(await gachaContract.balanceOf(account1.address, EVOLVED_TOKEN_ID)).to.equal(1);
   });
 
   it("Fails to evolve, missing base token", async function () {
-    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(0);
+    const [owner, account1] = await ethers.getSigners();
 
+    // user claims energy token
+    let myAllowlistProof: AllowlistProof = {
+      proof: ["0x0000000000000000000000000000000000000000000000000000000000000000"],
+      quantityLimitPerWallet: 100,
+      pricePerToken: ethers.parseEther("0"),
+      currency: ETH_NATIVE_ADDRESS
+    };
+    await gachaContract.claim(account1.address, ENERGY_TOKEN_ID, "2", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x");
+    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(2);
+    expect(await gachaContract.balanceOf(account1.address, ENERGY_TOKEN_ID)).to.equal(2);
+
+    // user does not claim base token
+    // user fails to claims evolved token
+    await expect(gachaContract.claim(account1.address, EVOLVED_TOKEN_ID, "1", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x")
+    ).to.be.revertedWith('Gacha: not a base token holders.')
+    expect(await gachaContract.totalSupply(EVOLVED_TOKEN_ID)).to.equal(0);
+    expect(await gachaContract.balanceOf(account1.address, EVOLVED_TOKEN_ID)).to.equal(0);
   });
 
-  it("Fails to evolve, missing not enough energy tokens", async function () {
-    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(0);
+  it("Fails to evolve, not enough energy tokens", async function () {
+    const [owner, account1] = await ethers.getSigners();
 
+    // user claims energy token
+    let myAllowlistProof: AllowlistProof = {
+      proof: ["0x0000000000000000000000000000000000000000000000000000000000000000"],
+      quantityLimitPerWallet: 100,
+      pricePerToken: ethers.parseEther("0"),
+      currency: ETH_NATIVE_ADDRESS
+    };
+    await gachaContract.claim(account1.address, ENERGY_TOKEN_ID, "1", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x");
+    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(1);
+    expect(await gachaContract.balanceOf(account1.address, ENERGY_TOKEN_ID)).to.equal(1);
+
+    // user claims base token
+    await gachaContract.claim(account1.address, BASE_TOKEN_ID, "1", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x");
+    expect(await gachaContract.totalSupply(BASE_TOKEN_ID)).to.equal(1);
+    expect(await gachaContract.balanceOf(account1.address, BASE_TOKEN_ID)).to.equal(1);
+
+    // user fails to claims evolved token
+    await expect(gachaContract.claim(account1.address, EVOLVED_TOKEN_ID, "1", ETH_NATIVE_ADDRESS, 0, myAllowlistProof, "0x")
+    ).to.be.revertedWith('Gacha: not enough energy tokens.')
+    expect(await gachaContract.totalSupply(EVOLVED_TOKEN_ID)).to.equal(0);
+    expect(await gachaContract.balanceOf(account1.address, EVOLVED_TOKEN_ID)).to.equal(0);
   });
 
 });
