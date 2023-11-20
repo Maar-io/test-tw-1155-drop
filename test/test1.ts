2b41c1f81f2b94c678b044d507060e48e0dc02f1
Mar.io <mario@stake.co.jp>
2023-11-20 18:05:32 +0100

    test working flow

--- test/test1.ts
+++ test/test1.ts
@@ -1,107 +1,54 @@
-import  { expect } from "chai";
+import { expect } from "chai";
 import { BigNumberish, BytesLike } from "ethers";
 import { ethers } from "hardhat";
 
 const ENERGY_TOKEN_ID = 0;
-// const CLAIM_CONDITION_0 = [
+const BASE_TOKEN_ID = 1;
+const EVOLVED_TOKEN_ID = 2;
 
-  
-  interface ClaimCondition {
-    startTimestamp: number;
-    maxClaimableSupply: number;
-    supplyClaimed: number;
-    quantityLimitPerWallet: number;
-    merkleRoot: BytesLike;
-    pricePerToken: BigNumberish;
-    currency: string;
-    metadata: string;
-  }
-  // interface ClaimCondition {
-  //   uint256 startTimestamp;
-  //   uint256 maxClaimableSupply;
-  //   uint256 supplyClaimed;
-  //   uint256 quantityLimitPerWallet;
-  //   bytes32 merkleRoot;
-  //   uint256 pricePerToken;
-  //   address currency;
-  //   string metadata;
-  // }
+type AllowlistProof = {
+  proof: string[];
+  quantityLimitPerWallet: number;
+  pricePerToken: BigNumberish;
+  currency: string;
+}
 
-  const claimConditions: {
-    _conditions: ClaimCondition[];
-  } = {
-    _conditions: [
-      {
-        startTimestamp: Math.floor(Date.now() / 1000),
-        maxClaimableSupply: 100,
-        supplyClaimed: 0,
-        quantityLimitPerWallet: 10,
-        merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
-        pricePerToken: ethers.parseEther("0.0"),
-        currency: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-        metadata: "ipfs://QmVu98eczZRpSYcF3UKYRDkHsM2RMQR62KUYmk29UDbWTP/0",
-      }
-    ],
-  };
-  
-  
-  
-//   {
-//     type: "BigNumber",
-//     hex: "0x65574bbf",
-//   },
-//   {
-//     type: "BigNumber",
-//     hex: "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-//   },
-//   {
-//     type: "BigNumber",
-//     hex: "0x04",
-//   },
-//   {
-//     type: "BigNumber",
-//     hex: "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-//   },
-//   "0x0000000000000000000000000000000000000000000000000000000000000000",
-//   {
-//     type: "BigNumber",
-//     hex: "0x00",
-//   },
-//   "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-//   "ipfs://QmVu98eczZRpSYcF3UKYRDkHsM2RMQR62KUYmk29UDbWTP/0",
-// ];
-// const CLAIM_CONDITION_1 = [
-//   {
-//     type: "BigNumber",
-//     hex: "0x65574bbf",
-//   },
-//   {
-//     type: "BigNumber",
-//     hex: "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-//   },
-//   {
-//     type: "BigNumber",
-//     hex: "0x01",
-//   },
-//   {
-//     type: "BigNumber",
-//     hex: "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-//   },
-//   "0x0000000000000000000000000000000000000000000000000000000000000000",
-//   {
-//     type: "BigNumber",
-//     hex: "0x00",
-//   },
-//   "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-//   "ipfs://QmVu98eczZRpSYcF3UKYRDkHsM2RMQR62KUYmk29UDbWTP/0",
-// ];
+interface ClaimCondition {
+  startTimestamp: number;
+  maxClaimableSupply: number;
+  supplyClaimed: number;
+  quantityLimitPerWallet: number;
+  merkleRoot: BytesLike;
+  pricePerToken: BigNumberish;
+  currency: string;
+  metadata: string;
+}
+
+const claimConditions: {
+  _conditions: ClaimCondition[];
+} = {
+  _conditions: [
+    {
+      startTimestamp: Math.floor(Date.now() / 1000),
+      maxClaimableSupply: 100,
+      supplyClaimed: 0,
+      quantityLimitPerWallet: 10,
+      merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+      pricePerToken: ethers.parseEther("0"),
+      currency: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+      metadata: "ipfs://QmVu98eczZRpSYcF3UKYRDkHsM2RMQR62KUYmk29UDbWTP/0",
+    }
+  ],
+};
 
 describe("Token contract", function () {
-  it("Set claim conditions", async function () {
+  let gachaContract: any;
+
+
+  beforeEach(async () => {
     const [owner] = await ethers.getSigners();
     const myContract = await ethers.getContractFactory("MyContract");
-
-    const gachaContract = await myContract.deploy(
+    gachaContract = await myContract.deploy(
       owner.address,
       "Gacha",
       "GACHA",
@@ -109,18 +56,67 @@ describe("Token contract", function () {
       100,
       owner.address
     );
-    const ownerBalance = await gachaContract.balanceOf(
-      owner.address,
-      ENERGY_TOKEN_ID
-    );
-    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(
-      ownerBalance
-    );
+    // lazy mint energy token
+    await gachaContract.lazyMint("0x01", "ipfs://QmNb8t2z16ztSg7SRfieQcBd9ARUy4Pp1XRbRsam6cG6Zg/", "0x");
+    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(0);
 
-    const condition = await gachaContract.setClaimConditions(0, claimConditions._conditions[0], false);
-    await condition.wait();
+    // lazy mint base token
+    await gachaContract.lazyMint("0x01", "ipfs://QmNb8t2z16ztSg7SRfieQcBd9ARUy4Pp1XRbRsam6cG6Zg/", "0x");
+    expect(await gachaContract.totalSupply(BASE_TOKEN_ID)).to.equal(0);
+
+    // lazy mint evolved token
+    await gachaContract.lazyMint("0x01", "ipfs://QmNb8t2z16ztSg7SRfieQcBd9ARUy4Pp1XRbRsam6cG6Zg/", "0x");
+    expect(await gachaContract.totalSupply(EVOLVED_TOKEN_ID)).to.equal(0);
+
+    // set and verify claim conditions for token ENERGY_TOKEN_ID
+    const condition0 = await gachaContract.setClaimConditions(ENERGY_TOKEN_ID, claimConditions._conditions[0], false);
+    await condition0.wait();
     const res = await gachaContract.claimCondition(0);
-    console.log(res);
-    const mintRes = await gachaContract.claim(0, owner.address, 1, "0x00000000)
+    expect(res[6]).to.equal("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
+
+    // set claim conditions for token BASE_TOKEN_ID
+    const condition1 = await gachaContract.setClaimConditions(BASE_TOKEN_ID, claimConditions._conditions[0], false);
+    await condition1.wait();
+
+    // set claim conditions for token BASE_TOKEN_ID
+    const condition2 = await gachaContract.setClaimConditions(EVOLVED_TOKEN_ID, claimConditions._conditions[0], false);
+    await condition2.wait();
+
   });
+
+  it("Evolution works", async function () {
+    const [owner, account1, account2] = await ethers.getSigners();
+
+    // user claims energy token
+    let myAllowlistProof: AllowlistProof = {
+      proof: ["0x0000000000000000000000000000000000000000000000000000000000000000"],
+      quantityLimitPerWallet: 100,
+      pricePerToken: ethers.parseEther("0"),
+      currency: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+    };
+    await gachaContract.claim(account1.address, ENERGY_TOKEN_ID, "2", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", 0, myAllowlistProof, "0x");
+    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(2);
+    expect(await gachaContract.balanceOf(account1.address, ENERGY_TOKEN_ID)).to.equal(2);
+
+    // user claims base token
+    await gachaContract.claim(account1.address, BASE_TOKEN_ID, "1", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", 0, myAllowlistProof, "0x");
+    expect(await gachaContract.totalSupply(BASE_TOKEN_ID)).to.equal(1);
+    expect(await gachaContract.balanceOf(account1.address, BASE_TOKEN_ID)).to.equal(1);
+
+    // user claims evolved token
+    await gachaContract.claim(account1.address, EVOLVED_TOKEN_ID, "1", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", 0, myAllowlistProof, "0x");
+    expect(await gachaContract.totalSupply(EVOLVED_TOKEN_ID)).to.equal(1);
+    expect(await gachaContract.balanceOf(account1.address, EVOLVED_TOKEN_ID)).to.equal(1);
+  });
+
+  it("Fails to evolve, missing base token", async function () {
+    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(0);
+
+  });
+
+  it("Fails to evolve, missing not enough energy tokens", async function () {
+    expect(await gachaContract.totalSupply(ENERGY_TOKEN_ID)).to.equal(0);
+
+  });
+
 });
